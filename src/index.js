54ac05f84156d1acd4bdd27d10f7741eeaf7c259
Thiago Cabral <thiago231286@gmail.com>
2020-11-27 14:30:10 -0200

    Adicionado .env no gitignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 const app = require('./app');
 
-app.listen(process.env.API_PORT, () => console.log(`Server API W2Bit Bus Station rodando na porta ${process.env.API_PORT || 3000} `));
+app.listen(process.env.API_PORT || 3000, () => console.log('Running Server API W2Bit Bus Station'));
