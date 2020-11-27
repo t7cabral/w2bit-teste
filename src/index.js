a2de0311d3332457ca81fb058c70e9f2be66ca20
Thiago Cabral <thiago231286@gmail.com>
2020-11-27 14:13:36 -0200

    Configurado Procfile Heroku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 const app = require('./app');
 
-app.listen(process.env.API_PORT, () => console.log(`Server API W2Bit Bus Station rodando na porta ${process.env.API_PORT} `));
+app.listen(process.env.API_PORT, () => console.log(`Server API W2Bit Bus Station rodando na porta ${process.env.API_PORT || 3000} `));
