8765c3ba5bf34c95a6c4e4f96ef05e4697707373
Thiago Cabral <thiago231286@gmail.com>
2020-11-27 18:33:24 -0200

    Heroku config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 const app = require('./app');
 
-app.listen(process.env.API_PORT || 3000, () => console.log('Running Server API W2Bit Bus Station'));
+app.listen(process.env.PORT || 3000, () => console.log('Running Server API W2Bit Bus Station'));
