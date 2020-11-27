94ea6aa6cc1df7c25badfc6362610dc963d30a8b
Thiago Cabral <thiago231286@gmail.com>
2020-11-27 14:46:53 -0200

    Adicionado cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
  
 require('dotenv').config();
 const app = require('express')();
+const cors = require('cors')
 const bodyParser = require("body-parser");
-
 const routerAuth = require('./routes/auth');
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
